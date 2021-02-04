02a53a02e10c92f05974435d27d9cfbb1e650373
Térence Chateigné <terence.chateigne@posteo.net>
2021-02-04 19:20:46 +0100

    chat room screen + navigation

--- components/ChatListItem/index.tsx
+++ components/ChatListItem/index.tsx
@@ -1,8 +1,9 @@
 import moment from "moment";
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { ChatRoom } from "../../types";
 import styles from "./style";
+import { useNavigation } from "@react-navigation/native";
 
 export type ChatListItemProps = {
   chatRoom: ChatRoom;
@@ -10,26 +11,34 @@ export type ChatListItemProps = {
 
 const ChatListItem = ({ chatRoom }: ChatListItemProps) => {
   const user = chatRoom.users[1];
-  return (
-    <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <Image source={{ uri: user.imageUri }} style={styles.avatar} />
-        <View style={styles.midContainer}>
-          <Text style={styles.username}>{user.name}</Text>
-          <Text
-            numberOfLines={1}
-            ellipsizeMode="head"
-            style={styles.lastMessage}
-          >
-            {chatRoom.lastMessage.content}
-          </Text>
-        </View>
-      </View>
 
-      <Text style={styles.time}>
-        {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
-      </Text>
-    </View>
+  const navigation = useNavigation();
+
+  const onClick = () => {
+    navigation.navigate("ChatRoom", { id: chatRoom.id, name: user.name });
+  };
+  return (
+    <TouchableWithoutFeedback onPress={onClick}>
+      <View style={styles.container}>
+        <View style={styles.leftContainer}>
+          <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+          <View style={styles.midContainer}>
+            <Text style={styles.username}>{user.name}</Text>
+            <Text
+              numberOfLines={1}
+              ellipsizeMode="head"
+              style={styles.lastMessage}
+            >
+              {chatRoom.lastMessage.content}
+            </Text>
+          </View>
+        </View>
+
+        <Text style={styles.time}>
+          {moment(chatRoom.lastMessage.createdAt).format("DD/MM/YYYY")}
+        </Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,12 +8,18 @@ import * as React from "react";
 import { ColorSchemeName } from "react-native";
 import { View } from "../components/Themed";
 import Colors from "../constants/Colors";
-import { Octicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  Octicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  FontAwesome5,
+} from "@expo/vector-icons";
 
 import NotFoundScreen from "../screens/NotFoundScreen";
 import { RootStackParamList } from "../types";
 import MainTabNavigator from "./MainTabNavigator";
 import LinkingConfiguration from "./LinkingConfiguration";
+import ChatRoomScreen from "../screens/ChatRoomScreen";
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -76,6 +82,32 @@ function RootNavigator() {
             </View>
           ),
         }}
+      />
+      <Stack.Screen
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View
+              style={{
+                backgroundColor: "transparent",
+                flexDirection: "row",
+                width: 100,
+                justifyContent: "space-between",
+                marginRight: 10,
+              }}
+            >
+              <MaterialIcons name="call" size={22} color="white" />
+              <FontAwesome5 name="video" size={22} color="white" />
+              <MaterialCommunityIcons
+                name="dots-vertical"
+                size={22}
+                color="white"
+              />
+            </View>
+          ),
+        })}
       />
       <Stack.Screen
         name="NotFound"
